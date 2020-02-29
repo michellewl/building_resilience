344f09127f5f84fe8980da62894bca7d05721adf
oremnirv <omernivr12+git@gmail.com>
2020-02-29 15:19:12 +0000

    added sh file to create directories for climate model output

---
+++ jasmin/create_directories_climate_models.sh
@@ -0,0 +1,4 @@
+mkdir -p HadGEM2-CC/{historical_r1, historical_r2, historical_r3, piControl_r1, rcp45_r1, rcp85_r1, rcp85_r2, rcp85_r3}
+mkdir -p bcc-csm1-1-m/{historical_r1, historical_r2, historical_r3, piControl_r1, rcp26_r1, rcp45_r1, rcp85_r1}
+mkdir -p CanESM2/{historical_r1, historical_r2, historical_r3, piControl_r1, rcp26_r1, rcp26_r2, rcp26_r3, rcp45_r1, rcp85_r1}
+mkdir -p BNU-ESM/{historical_r1, piControl_r1, rcp26_r1, rcp45_r1, rcp85_r1}
