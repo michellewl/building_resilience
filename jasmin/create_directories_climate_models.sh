e3f6b28ebe4d467f96ff3f9cb0dc3a4bd0339581
oremnirv <omernivr12+git@gmail.com>
2020-03-02 22:07:01 +0000

    creating directories sh file now works!

--- jasmin/create_directories_climate_models.sh
+++ jasmin/create_directories_climate_models.sh
@@ -1,8 +1,9 @@
-mkdir -p HadGEM2-CC/{historical_r1, historical_r2, historical_r3, piControl_r1, rcp45_r1, rcp85_r1, rcp85_r2, rcp85_r3}
-mkdir -p bcc-csm1-1-m/{historical_r1, historical_r2, historical_r3, piControl_r1, rcp26_r1, rcp45_r1, rcp85_r1}
-mkdir -p CanESM2/{historical_r1, historical_r2, historical_r3, piControl_r1, rcp26_r1, rcp26_r2, rcp26_r3, rcp45_r1, rcp85_r1}
-mkdir -p BNU-ESM/{historical_r1, piControl_r1, rcp26_r1, rcp45_r1, rcp85_r1}
+mkdir -p HadGEM2-CC/historical_r1 HadGEM2-CC/historical_r2 HadGEM2-CC/historical_r3 HadGEM2-CC/piControl_r1 HadGEM2-CC/rcp45_r1 HadGEM2-CC/rcp85_r1 HadGEM2-CC/rcp85_r2 HadGEM2-CC/rcp85_r3
+mkdir -p bcc-csm1-1-m/historical_r1 bcc-csm1-1-m/historical_r2 bcc-csm1-1-m/historical_r3 bcc-csm1-1-m/piControl_r1 bcc-csm1-1-m/rcp26_r1 bcc-csm1-1-m/rcp45_r1 bcc-csm1-1-m/rcp85_r1
+mkdir -p CanESM2/historical_r1 CanESM2/historical_r2 CanESM2/historical_r3 CanESM2/piControl_r1 CanESM2/rcp26_r1 CanESM2/rcp26_r2 CanESM2/rcp26_r3 CanESM2/rcp45_r1 CanESM2/rcp85_r1
+mkdir -p BNU-ESM/historical_r1 BNU-ESM/piControl_r1 BNU-ESM/rcp26_r1 BNU-ESM/rcp45_r1 BNU-ESM/rcp85_r1
 mkdir ERA
+
 
 cd ~/PYTHON/bcc-csm1-1-m/rcp26_r1/
 mkdir mean_bias delta ecdf
