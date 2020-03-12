b9bd78ee702534618287fd4dd853454b8fcc40c0
Omer Nivron <omernivron@dhcp-10-249-169-44.eduroam.wireless.private.cam.ac.uk>
2020-03-12 14:32:48 +0000

    metrics with some explanations

--- models/metrics.py
+++ models/metrics.py
@@ -4,6 +4,9 @@ import numpy as np
 
 def coef_of_var_metric(obs, pred):
     '''
+    normalizes the prediction error by the average energy consumption 
+    and provides a unitless measure that is more convenient for comparison purposes
+    --------
     obs (array of floats)
     pred (array of floats)
     -------
@@ -31,7 +34,13 @@ def rmse(obs, pred):
 
 
 def smape(obs, pred):
+
     '''
+    Accuracy measure based on percentage (or relative) errors.
+    Percentage error between 0% and 100%
+    not symmetric.
+    e.g. The difference between 0 and 1 isn't the same as 100 and 101!   
+    ---------
     obs (array of floats)
     pred (array of floats)
     -------
@@ -41,4 +50,4 @@ def smape(obs, pred):
     
     '''
 	n = len(obs)
-	return np.sum(np.abs(pred - obs) /  ((np.abs(obs) + np.abs(pred)) / 2)) * 100 / n
+	return np.sum(np.abs(pred - obs) /  ((np.abs(obs) + np.abs(pred)) )) * 100 / n
