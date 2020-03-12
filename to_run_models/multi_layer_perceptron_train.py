64adff40d7043bc8825110d3ee3451f14fadf57b
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-03-12 19:52:30 +0000

    Ran MLP on daily data.

--- to_run_models/multi_layer_perceptron_train.py
+++ to_run_models/multi_layer_perceptron_train.py
@@ -34,7 +34,7 @@ write(title, f"Training array dimensions: {X_train.shape} {y_train.shape}")
 write(title, f"Test array dimensions: {X_test.shape} {y_test.shape}")
 
 print("Fitting multi-layer perceptron regression model...")
-model = MLPRegressor(hidden_layer_sizes=(100,100,), batch_size=16, verbose=True, max_iter=200)
+model = MLPRegressor(hidden_layer_sizes=(50,50,), batch_size=16, verbose=True, max_iter=200)
 write(title, f"{current_time()}\nArchitecture: {model.hidden_layer_sizes}\nBatch size: {model.batch_size}")
 model.fit(X_train,y_train)
 
@@ -46,7 +46,7 @@ write(title, f"\n{current_time()}\n\nBest loss: {model.best_loss_}"
              f"\nOutput activation function: {model.out_activation_}"
              f"\nSolver: {model.solver}")
 
-arch = "_100_100"
+arch = "_50_50"
 filename = f"{code_home_folder}models/MLP_model_monthly{arch}.sav"
 pickle.dump(model, open(filename, "wb"))
 print("Saved model.")
