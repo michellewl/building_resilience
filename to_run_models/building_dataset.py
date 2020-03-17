0c579daaed209156dbc2948b2e592d13252d9dc7
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-03-17 20:52:27 +0000

    Finished defining building dataset class.

--- to_run_models/building_dataset.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.utils.data
-
-class BuildingDataset(Dataset):
-    def __init__(self, X_filepath, y_filepath):
-        self.inputs = torch.from_numpy(np.load(X_filepath)).float()
-        self.targets = torch.
-
