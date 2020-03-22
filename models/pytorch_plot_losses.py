d4f3862079c390022e1a37c224b9544e3eeced90
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-03-22 18:33:58 +0000

    Plots log of loss for training and validation.

--- models/pytorch_plot_losses.py
+++ models/pytorch_plot_losses.py
@@ -6,11 +6,14 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from datasets.building_dataset import BuildingDataset
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 windows_os = True
 
-arch = "_100_100"
+hidden_layer_1 = 100
+hidden_layer_2 = 100
+
+arch = f"_{hidden_layer_1}_{hidden_layer_2}"
 
 if windows_os:
     code_home_folder = "C:\\Users\\Michelle\\OneDrive - University of Cambridge\\MRes\\Guided_Team_Challenge\\building_resilience\\"
@@ -23,9 +26,13 @@ else:
     data_folder = "data/train_test_arrays/"
     filename = f"{code_home_folder}models/saved/MLP_pytorch_model_daily{arch}.tar"
 
-epoch_losses = torch.load(filename)["training_loss_history"]
+training_losses = torch.load(filename)["training_loss_history"]
+val_losses = torch.load(filename)["validation_loss_history"]
 
-print(epoch_losses)
-
-# plt.plot(range(len(epoch_losses)), epoch_losses)
-# plt.show()
+fig, ax = plt.subplots()
+ax.plot(range(len(training_losses)), np.log(training_losses), label="training loss")
+ax.plot(range(len(val_losses)), np.log(val_losses), label="validation loss")
+plt.legend()
+plt.xlabel("epoch")
+plt.ylabel("log(loss)")
+plt.show()
