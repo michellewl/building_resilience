9499e2b1afad749c6973db8a5d4f3912e3ab97b7
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-03-22 14:55:16 +0000

    Create script to plot training loss of epochs.

---
+++ to_run_models/pytorch_plot_losses.py
@@ -0,0 +1,32 @@
+from functions.functions import write, current_time
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from datasets.building_dataset import BuildingDataset
+import matplotlib.pyplot as plt
+
+
+windows_os = True
+
+arch = "_100_100"
+
+if windows_os:
+    code_home_folder = "C:\\Users\\Michelle\\OneDrive - University of Cambridge\\MRes\\Guided_Team_Challenge\\building_resilience\\"
+    title = f"{code_home_folder}logs\\training\\daily_data\\MLP_pytorch_log_{current_time()}"
+    data_folder = "data\\train_test_arrays\\"
+    filename = f"{code_home_folder}models\\MLP_pytorch_model_daily{arch}.tar"
+else:
+    code_home_folder = "/home/mwlw3/Documents/Guided_team_challenge/building_resilience/"
+    title = f"{code_home_folder}logs/training/daily_data/MLP_log_{current_time()}"
+    data_folder = "data/train_test_arrays/"
+    filename = f"{code_home_folder}models/MLP_pytorch_model_daily{arch}.tar"
+
+model = torch.load(filename)
+epoch_losses = model["loss_history"]
+
+print(epoch_losses)
+
+# plt.plot(range(len(epoch_losses)), epoch_losses)
+# plt.show()
