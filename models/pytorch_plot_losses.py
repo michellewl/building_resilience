1a8ebd61ceaa1f1f62a1d9e40f4f19b5baf87526
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-03-23 11:21:43 +0000

    Tidied up code.

--- models/pytorch_plot_losses.py
+++ models/pytorch_plot_losses.py
@@ -1,10 +1,4 @@
-from functions.functions import write, current_time
-
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from datasets.building_dataset import BuildingDataset
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -18,13 +12,9 @@ arch = f"_{hidden_layer_1}_{hidden_layer_2}_{hidden_layer_3}"
 
 if windows_os:
     code_home_folder = "C:\\Users\\Michelle\\OneDrive - University of Cambridge\\MRes\\Guided_Team_Challenge\\building_resilience\\"
-    title = f"{code_home_folder}logs\\training\\daily_data\\MLP_pytorch_log_{current_time()}"
-    data_folder = "data\\train_test_arrays\\"
     filename = f"{code_home_folder}models\\saved\\MLP_pytorch_model_daily{arch}.tar"
 else:
     code_home_folder = "/home/mwlw3/Documents/Guided_team_challenge/building_resilience/"
-    title = f"{code_home_folder}logs/training/daily_data/MLP_pytorch_log_{current_time()}"
-    data_folder = "data/train_test_arrays/"
     filename = f"{code_home_folder}models/saved/MLP_pytorch_model_daily{arch}.tar"
 
 training_losses = torch.load(filename)["training_loss_history"]
