8041d11ae6a2410a03d89302db65a255e170d196
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-03-23 11:18:40 +0000

    Added a copy network with 3 hidden layers, no batch norm. Main network has 4 hidden layers with batch norm.

--- models/neural_networks/multilayer_perceptron.py
+++ models/neural_networks/multilayer_perceptron.py
@@ -22,3 +22,20 @@ class SimpleNet(nn.Module):
         x = torch.relu(self.bn4(self.fc4(x)))
         x = self.fc5(x)
         return x
+
+
+class SimpleNet_3(nn.Module):
+    def __init__(self, number_of_features, hl1, hl2, hl3):
+        super(SimpleNet_3, self).__init__()
+
+        self.fc1 = nn.Linear(number_of_features, hl1)
+        self.fc2 = nn.Linear(hl1, hl2)
+        self.fc3 = nn.Linear(hl2, hl3)
+        self.fc4 = nn.Linear(hl3, 1)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
