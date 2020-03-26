4b537ad1bfa3d7dedae271f8eb2c67260fe8c2e6
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-03-26 13:13:26 +0000

    Create plot of model evaluation with SMAPE metric.

--- models/plot_model_evaluation.py
+++ models/plot_model_evaluation.py
@@ -22,23 +22,31 @@ eval_table = pd.read_csv(filepath)
 
 models_list = glob(f"{models_folder}*.tar")
 
+# Create new column in evaluation table dataframe
+eval_table["n_parameters"] = ""
 
+# Fill the table for the linear regression model
+eval_table.loc[eval_table.model_type == "linear_regression", "n_parameters"] = 49+1
 
+# Fill the table for the MLP models
 for model_name in models_list:
     model = NNetwork(model_name)
-    print(f"\nNumber of hidden layers: {model.hidden_layers}"
-          f"\nNumber of nodes per hidden layer: {model.nodes}"
-          f"\nNumber of parameters: {model.n_parameters}")
+    eval_table.loc[(eval_table.model_type == "multilayer_perceptron")
+                   & (eval_table.hidden_layers == model.hidden_layers)
+                   & (eval_table.nodes == model.nodes[0]), "n_parameters"] = model.n_parameters
 
+# Use a log scale
+eval_table["log_n_params"] = np.log(eval_table.n_parameters.astype('float64'))
 
-#
-# fig, ax = plt.subplots()
-# ax.scatter(eval_table.loc[eval_table.model_type == "multilayer_perceptron"].index,
-#            eval_table.loc[eval_table.model_type == "multilayer_perceptron"].smape, label="neural network")
-# ax.scatter(eval_table.loc[eval_table.model_type == "linear_regression"].index,
-#            eval_table.loc[eval_table.model_type == "linear_regression"].smape, label="linear regression")
-# plt.ylim(0, 100)
-# plt.ylabel("% error (SMAPE metric)")
-# plt.xlabel("Model complexity / arb. units")
-# plt.legend()
-# plt.show()
+fig, ax = plt.subplots()
+ax.scatter(eval_table.loc[eval_table.model_type == "multilayer_perceptron"].log_n_params,
+           eval_table.loc[eval_table.model_type == "multilayer_perceptron"].smape, label="neural network")
+ax.scatter(eval_table.loc[eval_table.model_type == "linear_regression"].log_n_params,
+           eval_table.loc[eval_table.model_type == "linear_regression"].smape, label="linear regression")
+plt.ylim(0, 100)
+plt.ylabel("symmetric mean absolute percentage error")
+plt.xlabel("log(number of parameters)")
+plt.title("% error")
+plt.legend()
+
+fig.savefig(f"{folder}model_evaluation.png", dpi=200)
