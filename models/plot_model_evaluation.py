4cde79346c79ef08973a9904aef5386112c0617a
Michelle WL Wan <mwlw3@cam.ac.uk>
2020-03-26 12:36:19 +0000

    Moved class definition to separate python file.

--- models/plot_model_evaluation.py
+++ models/plot_model_evaluation.py
@@ -2,10 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from glob import glob
-from functions.functions import get_models_by_hidden_layers
-import torch
-import re
-from neural_networks.multilayer_perceptron import SimpleNet_2, SimpleNet_3, SimpleNet_4
+from functions.classes import NNetwork
+
 
 windows_os = True
 
@@ -24,31 +22,6 @@ eval_table = pd.read_csv(filepath)
 
 models_list = glob(f"{models_folder}*.tar")
 
-two_hl_list = get_models_by_hidden_layers(models_list, 2)
-three_hl_list = get_models_by_hidden_layers(models_list, 3)
-four_hl_list = get_models_by_hidden_layers(models_list, 4)
-
-
-class NNetwork():
-    def __init__(self, filename):
-        self.filename = filename
-        self.nodes = list(map(int, re.compile(r'\d+').findall(filename)))
-        self.hidden_layers = len(self.nodes)
-
-        if self.hidden_layers == 2:
-            self.model = SimpleNet_2(49, self.nodes[0], self.nodes[1])
-        elif self.hidden_layers ==3:
-            self.model = SimpleNet_3(49, self.nodes[0], self.nodes[1], self.nodes[2])
-        elif self.hidden_layers ==4:
-            self.model = SimpleNet_4(49, self.nodes[0], self.nodes[1], self.nodes[2], self.nodes[3])
-
-        self.n_parameters = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
-
-# two_hl = NNetwork(two_hl_list[0])
-# print(two_hl.hidden_layers, two_hl.nodes)
-# print(two_hl.nodes[0])
-# print(two_hl.n_parameters)
-#
 
 
 for model_name in models_list:
