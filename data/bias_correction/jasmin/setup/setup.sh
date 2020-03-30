c4ab9ae477624d3440f47dc96058cc72bd92247c
Omer Nivron <48557935+oremnirv@users.noreply.github.com>
2020-03-30 11:57:40 -0400

    Update setup.sh

--- data/bias_correction/jasmin/setup/setup.sh
+++ data/bias_correction/jasmin/setup/setup.sh
@@ -2,6 +2,5 @@ mkdir ~/PYTHON
 export PYTHONPATH="$HOME/PYTHON"  # <- add to ~/.bashrc
 cd $PYTHONPATH
 git clone https://github.com/scott-hosking/baspy.git
-mv ../building_resilience/data/bias_correction/jasmin/downloader/dataprocessing.py ./
-mv ../building_resilience/data/bias_correction/jasmin/downloader/helper.py ./
+cp -r ../building_resilience/data/bias_correction/jasmin/downloader ./
 mkdir ERA
