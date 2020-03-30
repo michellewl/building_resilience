fc345efbc3ccded4417242f26b899b6ff5d326a8
Omer Nivron <48557935+oremnirv@users.noreply.github.com>
2020-03-30 15:33:45 -0400

    Update era_bjob.sh

--- data/bias_correction/jasmin/bjobs/era_bjob.sh
+++ data/bias_correction/jasmin/bjobs/era_bjob.sh
@@ -1,1 +1,1 @@
-for i in {3..8}; do bsub < ../building_resilience/data/bias_correction/jasmin/bjobs/era/era${i}.bsub; done
+for i in {3..8}; do bsub < ~/building_resilience/data/bias_correction/jasmin/bjobs/era/era${i}.bsub; done
