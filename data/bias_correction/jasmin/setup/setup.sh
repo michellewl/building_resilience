0b688de857030dc0e6755a573c53599f30405e48
Omer Nivron <48557935+oremnirv@users.noreply.github.com>
2020-03-30 12:08:42 -0400

    Update setup.sh

--- data/bias_correction/jasmin/setup/setup.sh
+++ data/bias_correction/jasmin/setup/setup.sh
@@ -4,4 +4,4 @@ cd $PYTHONPATH
 git clone https://github.com/scott-hosking/baspy.git
 cp -r ../building_resilience/data/bias_correction/jasmin/downloader ./
 mkdir ERA
-mkdir -p bjobs_run/model/era
+mkdir -p bjobs_run/model bjobs_run/era
